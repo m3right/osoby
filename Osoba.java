7462a7310874b7c507c0da9659d4d346e0182c50
Rafał Dreżewski <drezew@agh.edu.pl>
2021-11-24 22:07:10 +0100

    makefile

--- Osoba.java
+++ Osoba.java
@@ -1,13 +1,13 @@
 public class Osoba {
 	String imie;
 	String nazwisko;
-	
+
 	public Osoba(String imie, String nazwisko) {
 		this.imie = imie;
 		this.nazwisko = nazwisko;
 	}
-	
+
 	public void opis() {
-		System.out.println("Nazwywam sie " + imie + " " + nazwisko);
+		System.out.println("Nazywam sie " + imie + " " + nazwisko);
 	}
 }
